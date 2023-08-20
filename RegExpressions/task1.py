37bdf592d3fcf44facfe6a6573640f67c4ab22b1
pawel184 <skaklkijpasa@gmail.com>
2023-08-20 18:17:19 +0200

    RegExpressions

--- RegExpressions/task1.py
+++ RegExpressions/task1.py
@@ -2,20 +2,23 @@
 # Используем регулярные выражения для извлечения информации
 import re
 text = input("Введите информацию")
-
 match1 = re.search(r'D\w+', text)
 match2 = re.search(r'S\w+',text)
 match3 = re.search(r'\d+',text)
-match4 = re.search(r'G\w+',text)
+match4 = re.search(r'\d\d.\d\d.\d+',text)
+match5 = re.search(r'p\w+',text)
+match6 = re.search(r'g\w+',text)
 
 name = match1.group()
 surname = match2.group()
 age = match3.group()
-location = match4.group()
-
-    # Выводим информацию
+date= match4.group()
+location = match5.group()
+location2=match6.group()
 print(name)
 print(surname)
 print(age)
+print(date)
 print(location)
+print(location2)
 
