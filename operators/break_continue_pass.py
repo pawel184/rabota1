b1515e675e0401712cdbc7372892914bb5ae144b
pawel184 <skaklkijpasa@gmail.com>
2023-09-02 13:11:41 +0200

    operators

--- operators/break_continue_pass.py
+++ operators/break_continue_pass.py
@@ -5,12 +5,14 @@ while current_number <= 30:
         print("Это диапазон чисел от 1 до 7")
         for i in range(1, 8):
             print(i)
-        current_number = 8  
+        current_number = 8 
+        continue 
     elif  current_number <= 15:
         print("Это диапазон чисел от 10 до 15")
         for i in range(10, 16):
             print(i)
         current_number = 16 
+        continue
     elif  current_number <= 30:
         print("Это диапазон чисел от 25 до 30")
         for i in range(25, 31):
@@ -21,4 +23,4 @@ while current_number <= 30:
                 print(i)
         for i in range(16, 25):
             print(i)
-        current_number = 31
+        
